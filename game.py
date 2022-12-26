3c083afe8f617d5e04f2a46e5f32c94acc95eaab
Lorenzo Alencar Tomaz <lorenzotomaz@MacBook-Pro-de-Lorenzo.local>
2022-12-26 19:39:17 -0300

    added more descriptions

--- game.py
+++ game.py
@@ -19,7 +19,7 @@ openai.api_key = os.environ["API_KEY"]
 def ask(question: str):
     print("A man was found dead in the desert with a backpack. Find the cause of the death")
     riddle = "a man was found dead in the desert with a backpack."
-    answer = "The answer is that the backpack was holding a parachute, the parachute did not open and the man died."
+    answer = "The answer is the man was in airplan trying to jump with a backpack was holding a parachute, the parachute did not open and the man died.The man was airplane pilot. the man was using a brown shirt and yellow pants"
     base_prompt = "You are a professor in a university that teaches people how to become top-notch detectives. Now you need to evaluate a candidate that wants to become a detective. You must execute the following test: describe a murder scene to the candidate and ask him the cause of death which he can answer by asking questions that you can answer only with \"yes\", \"no\" or \"indifferent\". The candidate must inform the cause of death and if his answer is correct you must inform him that he passed the exam. You must tell the following history:" + riddle + answer + "You must not tell the answer to the candidate. You must wait for the candidate to ask questions before you answer him. You must not produce examples. You must answer \"indifferent\" when the question is out of topic. When the candidate informs the proper the cause of death you must say \"You passed the exam\"."
     user_prompt = base_prompt + "\nCandidate: " + question + "?\n" + "You: "
     response = openai.Completion.create(model="text-davinci-003", prompt=user_prompt)
